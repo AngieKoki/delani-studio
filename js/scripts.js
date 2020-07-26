129688ac709d1146944cc824ea23031d98aac782
Angie Koki <angie.dar66@gmail.com>
2020-07-26 16:28:47 +0300

    add toggle to what we do section add some padding on the what we do section

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,34 @@
 
 
 //FRONT-END LOGIC
+$(document).ready(function(){
+    $('.design').click(function(){
+        $('.design').toggle();
+        $('.showDesign').toggle();
+        $('p').click(function(){
+            $('p').hide();
+            $('.design').show();
+        });
+    });
+});
+$(document).ready(function(){
+    $('.development').click(function(){
+        $('.development').toggle();
+        $('.showDev').toggle();
+        $('p').click(function(){
+            $('p').hide();
+            $('.development').show();
+        });
+     
+    });   
+});
+$(document).ready(function(){
+    $('.product').click(function(){
+        $('.product').toggle();
+        $('.showProduct').toggle();
+        $('p').click(function(){
+            $('p').hide();
+            $('.product').show();
+        });
+    });
+});
